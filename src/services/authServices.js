3239915464f1885c1d091af1abe20895e6c7eea6
DARSHAN <darshandesale143@gmail.com>
2026-05-15 22:34:41 +0530

    Login problem sol

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "https://hms-deployment.onrender.com/auth";
+const API = "https://hms-deployment-eh4s.onrender.com/auth";
 
 
 export const register = (data) => {
