ad8a81badc981da60acdb6fb9e454b4bfdfa817f
DARSHAN <darshandesale143@gmail.com>
2026-05-25 14:31:26 +0530

    Removed duplicate frontend files

---
+++ backend/src/main/java/com/hms/config/ForwardController.java
@@ -0,0 +1,16 @@
+package com.hms.config;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ForwardController {
+
+    @RequestMapping(value = {
+            "/{path:[^\\.]*}",
+            "/**/{path:[^\\.]*}"
+    })
+    public String redirect() {
+        return "forward:/";
+    }
+}
